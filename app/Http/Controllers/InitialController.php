12b7be38a89dd92d0c757723c8796e99dde5a5ed
manishdixit18 <manishdixit18@gmail.com>
2018-02-06 12:14:33 +0530

    blank array in api failure

--- app/Http/Controllers/InitialController.php
+++ app/Http/Controllers/InitialController.php
@@ -12,7 +12,7 @@ class InitialController extends Controller
       }
       public function send_failure_response($message,$status,$data){
 
-      	$res = array('message' =>$message ,'status'=>$status,'status_code'=>1,'MasterData'=>$data );
+      	$res = array('message' =>$message ,'status'=>$status,'status_code'=>1,'MasterData'=>[] );
       	return Response::json($res);
       }
 }
